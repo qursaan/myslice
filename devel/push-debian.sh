1f7e87134e52eb2a24c9ca1d31c84c6fd7e778b6
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2012-11-16 08:38:46 +0100

    tweaks in the devel scripts

--- devel/push-debian.sh
+++ devel/push-debian.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR=$(dirname $0)
+ROOT=$(cd $DIR/.. ; pwd -P)
 
 host=debian03.pl.sophia.inria.fr
 
-rsync -a "$@" $DIR/ root@$host:/root/myslice/
+rsync -a "$@" --exclude '*.sqlite3' $ROOT/ root@$host:/root/myslice/
