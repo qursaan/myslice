484b16eca194516c6a14ad1e000eeef24aa41fd6
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2012-11-20 17:17:16 +0100

    enabled django-staticfiles and added logo

--- devel/push-debian.sh
+++ devel/push-debian.sh
@@ -2,6 +2,6 @@
 DIR=$(dirname $0)
 ROOT=$(cd $DIR/.. ; pwd -P)
 
-host=debian03.pl.sophia.inria.fr
+host=debian04.pl.sophia.inria.fr
 
 rsync -a "$@" --exclude '*.sqlite3' $ROOT/ root@$host:/root/myslice/
--- devel/server-loop.sh
+++ devel/server-loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIRNAME=$(dirname $0)
-cd $DIRNAME
+cd $DIRNAME/..
 
 # default port : if hostname starts with z -> use 8080 ; otherwise take 80
 hostname | grep -q '^z' && port=8080 || port=80
