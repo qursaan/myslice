20028a8cc2b83ed3c8d98218958ebd34bc75f891
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2012-12-10 16:57:58 +0100

    getting closer

--- auth/views.py
+++ auth/views.py
@@ -3,10 +3,15 @@ from django.core.context_processors import csrf
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+
+from auth.backend import MyCustomBackend
 
 def login_user(request):
     state = "Please log in below..."
     username = password = ''
+    env={'hard_wired_users':MyCustomBackend.hard_wired_users}
+    
     if request.POST:
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -16,10 +21,16 @@ def login_user(request):
             if user.is_active:
                 login(request, user)
                 state = "You're successfully logged in!"
+                return HttpResponseRedirect ('/')
             else:
                 state = "Your account is not active, please contact the site admin."
+                env['state']=state; env['username']=username
+                return render_to_response('view-login.html',env, context_instance=RequestContext(request))
         else:
             state = "Your username and/or password were incorrect."
-
-    return render_to_response('view-login.html',{'state':state, 'username': username},
-                              context_instance=RequestContext(request))
+            env['state']=state; env['username']=username
+            return render_to_response('view-login.html',env, context_instance=RequestContext(request))
+    else:
+        state='Welcome to MySlice'
+        env['state']=state; env['username']=''
+        return render_to_response('view-login.html',env, context_instance=RequestContext(request))
--- slice/views.py
+++ slice/views.py
@@ -5,6 +5,7 @@ from django.core.context_processors import csrf
 from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 
 lorem="""
 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod <code>mazim placerat</code> facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.
@@ -20,7 +21,6 @@ def menu_items (current):
     result=deepcopy(standard_menu_items)
     for d in result:
         if d['label'].lower().find(current)>=0: d['active']=True
-    print "menu_items(%s)=%s"%(current,result)
     return result
     
 
@@ -29,6 +29,7 @@ for site in [ 'inria', 'upmc' , 'ibbt' ]:
     for slice in [ 'foo', 'bar', 'tutu', 'test', 'omf', 'heartbeat' ]:
         hard_wired_slice_names.append ("ple.%s.%s"%(site,slice))
 
+@login_required
 def fake_slice_view (request, name=None):
     result=render_to_response('view-slice.html',
                               {'foo':'var_foo_set_in_view', 
@@ -41,13 +42,15 @@ def fake_slice_view (request, name=None):
 
     return result
 
+@login_required
 def tab_view (request):
-    return render_to_response ('tab-view.html',
+    return render_to_response ('view-tab.html',
                                { 'lorem': lorem,
                                  'menu_items': menu_items('tab'),
                                  },
                                context_instance=RequestContext(request))
 
+@login_required
 def scroll_view (request):
     return render_to_response ('view-scroll.html',
                                { 'lorem':lorem,
