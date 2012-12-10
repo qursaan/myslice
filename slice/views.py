d99a40907815986a049b0e7726b2599a2cf14843
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2012-12-10 18:25:29 +0100

    was trying to show user logged in - not working yet

--- slice/views.py
+++ slice/views.py
@@ -3,7 +3,6 @@ from copy import deepcopy
 
 from django.core.context_processors import csrf
 from django.template import RequestContext
-from django.template.loader import render_to_string
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
@@ -11,9 +10,9 @@ lorem="""
 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod <code>mazim placerat</code> facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.
 """
 
-standard_menu_items = [ { 'label':'Tab view', 'href': '/tab/'},
-                        { 'label':'Slice view',  'href': '/slice/'},
+standard_menu_items = [ { 'label':'Slice view',  'href': '/slice/'},
                         { 'label':'Scroll view', 'href': '/scroll/'},
+                        { 'label':'Tab view', 'href': '/tab/'},
                         ]
 
 login_out_items = { False: { 'label':'Login', 'href':'/login/'},
@@ -33,31 +32,44 @@ for site in [ 'inria', 'upmc' , 'ibbt' ]:
     for slice in [ 'foo', 'bar', 'tutu', 'test', 'omf', 'heartbeat' ]:
         hard_wired_slice_names.append ("ple.%s.%s"%(site,slice))
 
+def the_user (request):
+    "This code below is broken"
+    return 'user-xxx-name'
+    if not request.user.is_authenticated (): return ''
+    else: return request.user.username
+
 @login_required
 def fake_slice_view (request, name=None):
-    result=render_to_response('view-slice.html',
-                              {'foo':'var_foo_set_in_view', 
-                               'name':name,
-                               'slices': hard_wired_slice_names,
-                               'content_main' : lorem,
-                               'menu_items' : menu_items('slice',request),
-                               },
-                              context_instance=RequestContext(request))
+    title='Fake Slice Page'
+    if name: title += " for slice %s"%name
+    result=render_to_response ('view-slice.html',
+                               {'name':name,
+                                'title':title,
+                                'menu_items' : menu_items('slice',request),
+                                'username':the_user (request),
+                                'slices': hard_wired_slice_names,
+                                'content_main' : lorem,
+                                },
+                               context_instance=RequestContext(request))
 
     return result
 
 @login_required
 def tab_view (request):
     return render_to_response ('view-tab.html',
-                               { 'lorem': lorem,
-                                 'menu_items': menu_items('tab',request),
-                                 },
+                               {'title':'Page for playing with Tabs',
+                                'menu_items': menu_items('tab',request),
+                                'username':the_user (request),
+                                'lorem': lorem,                                
+                                },
                                context_instance=RequestContext(request))
 
 @login_required
 def scroll_view (request):
     return render_to_response ('view-scroll.html',
-                               { 'lorem':lorem,
-                                 'menu_items': menu_items('scroll',request),
-                                 },
+                               {'title':'Toy page for scrolling',
+                                'menu_items': menu_items('scroll',request),
+                                'username':the_user (request),
+                                'lorem':lorem,
+                                },
                                context_instance=RequestContext(request))
