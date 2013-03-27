8c5d47ae722261b24e30be0f70466c48fc2b7840
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-03-27 13:02:49 +0100

    minor tweak

--- plugins/hazelnut/hazelnut.js
+++ plugins/hazelnut/hazelnut.js
@@ -117,9 +117,8 @@
         /* Transforms the table into DataTable, and keep a pointer to it */
 	actual_options = {
             // Customize the position of Datatables elements (length,filter,button,...)
-            // http://datatables.net/release-datatables/examples/advanced_init/dom_toolbar.html
-            // http://www.datatables.net/forums/discussion/3914/adding-buttons-to-header-or-footer/p1
-	    sDom: "<'row-fluid'<'span4'l><'span7'f><'span1'r>>t<'row-fluid'<'span4'i><'span8'p>>",
+	    // we use a fluid row on top and another on the bottom, making sure we take 12 grid elt's each time
+	    sDom: "<'row-fluid'<'span1'r><'span4'l><'span7'f>>t<'row-fluid'<'span4'i><'span8'p>>",
             sPaginationType: 'bootstrap',
             // Handle the null values & the error : Datatables warning Requested unknown parameter
             // http://datatables.net/forums/discussion/5331/datatables-warning-...-requested-unknown-parameter/p2
