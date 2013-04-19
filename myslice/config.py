b984d442d926f3afb93bf4fd4adc8a90ec46b2a1
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-04-19 10:44:20 +0200

    documenting an early attempt to point at an api-v2 instance (too early in fact)

--- myslice/config.py
+++ myslice/config.py
@@ -1,7 +1,14 @@
 class Config:
 
     # production should use https of course
+
+# an old-fashioned API - that's what is currently only supported   
     manifold_url = "http://manifold.pl.sophia.inria.fr:7080/"
+# this one runs a new API, but currently (april 2013) there are missing
+# features; GetSession and GetPersons are still there (they should go away) and 
+# the code for retrieving metadata does not work as-is, and I have no clue 
+# what it should become anyways, so...
+#    manifold_url = "http://dev.myslice.info:7080/"
 
     # exporting these details to js
     @staticmethod
