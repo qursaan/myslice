eb9691015e43aa929eab9ed48f41a05363788a54
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-09-03 17:45:55 +0200

    take sliceview out of trash/

--- myslice/urls.py
+++ myslice/urls.py
@@ -11,6 +11,7 @@ add_to_builtins('insert_above.templatetags.insert_tags')
 
 # main entry point (set to the / URL)
 default_view='trash.pluginview.test_plugin_view'
+#default_view='portal.views.PlatformsView'
 # where to be redirected after login
 after_login_view='trash.dashboard.dashboard_view'
 
@@ -39,15 +40,17 @@ urlpatterns = patterns(
     # the manifold proxy
     #
     (r'^manifold/proxy/(?P<format>\w+)/?$', 'manifold.manifoldproxy.proxy'),
+    # 
+    # the slice view
     #
+    (r'^slice/?$',                        'portal.sliceview.slice_view'),
+    (r'^slice/(?P<slicename>[\w\.]+)/?$', 'portal.sliceview.slice_view'),
     # various trash views
     #
-    (r'^tab/?$', 'trash.sampleviews.tab_view'),
-    (r'^scroll/?$', 'trash.sampleviews.scroll_view'),
-    (r'^plugin/?$', 'trash.pluginview.test_plugin_view'),
-    (r'^dashboard/?$', 'trash.dashboard.dashboard_view'),
-    (r'^slice/?$', 'trash.sliceview.slice_view'),
-    (r'^slice/(?P<slicename>[\w\.]+)/?$', 'trash.sliceview.slice_view'),
+    (r'^tab/?$',                          'trash.sampleviews.tab_view'),
+    (r'^scroll/?$',                       'trash.sampleviews.scroll_view'),
+    (r'^plugin/?$',                       'trash.pluginview.test_plugin_view'),
+    (r'^dashboard/?$',                    'trash.dashboard.dashboard_view'),
     # Portal
     url(r'^portal/', include('portal.urls')),
     # Portal
