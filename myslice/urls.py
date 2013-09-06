13d84b91e3804fc228f51874896c86d8c6aa02f1
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-09-06 10:32:54 +0200

    trash-based urls are now prefixed in trash/ and moved in trash/urls.py

--- myslice/urls.py
+++ myslice/urls.py
@@ -65,9 +65,6 @@ urlpatterns = patterns(
     #
     # various trash views - bound to go away 
     #
-    (r'^tab/?$',                          'trash.sampleviews.tab_view'),
-    (r'^scroll/?$',                       'trash.sampleviews.scroll_view'),
-    (r'^plugin/?$',                       'trash.pluginview.test_plugin_view'),
-    (r'^dashboard/?$',                    'trash.dashboard.dashboard_view'),
+    url(r'^trash/', include('trash.urls')),
 
 )
