5d8da8c6bac88f51535c09411867de676dd8e99c
Jordan Augé <jordan.auge@lip6.fr>
2013-09-26 17:28:40 +0200

    updated some fixes for the demo

--- plugins/tabs/static/js/tabs.js
+++ plugins/tabs/static/js/tabs.js
@@ -2,7 +2,7 @@
 
     $.fn.Tabs = function( method ) {
 
-        $('a[data-toggle="tab"]').on('shown', function (e) {
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
           // find the plugin object inside the tab content referenced by the current tabs
           $('.plugin', $($(e.target).attr('href'))).trigger('show');
         });
