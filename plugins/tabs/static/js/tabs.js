9a486e42cde8ce57e6e12111423c49780c72ff1a
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-09-27 16:12:36 +0200

    typo & indented

--- plugins/tabs/static/js/tabs.js
+++ plugins/tabs/static/js/tabs.js
@@ -1,14 +1,10 @@
 (function($){
-
     $.fn.Tabs = function( method ) {
-
-        // In Bootstrap 3, we need shown.bs.tab instrad of shown.
+        // In Bootstrap 3, we need 'shown.bs.tab' instead of 'shown'
         // see: http://bootply.com/bootstrap-3-migration-guide
         $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
           // find the plugin object inside the tab content referenced by the current tabs
           $('.plugin', $($(e.target).attr('href'))).trigger('show');
         });
-
     };
-
 })( jQuery );
