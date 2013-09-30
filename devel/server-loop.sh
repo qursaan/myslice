31f84d68d3bd9208a91abc1f2f59389e1a0caca3
Anthony Garcia <anthony.garcia@inria.fr>
2013-09-30 12:55:32 +0200

    kill server when port is already used

--- devel/server-loop.sh
+++ devel/server-loop.sh
@@ -10,6 +10,11 @@ while true; do
     # we use make for convenience
     # but what the static target really does is to invoke manage.py collectstatic 
     make static templates
+
+    # if port is already used, we kill the server
+    pid=$(netstat -putan | awk "\$4 == \"0.0.0.0:$port\" && \$7 != \"-\" {split(\$7,a,\"/\"); print a[1]}")
+    [[ -n "$pid" ]] && kill $pid
+    
     ./manage.py runserver 0.0.0.0:$port
 	sleep 1
 done
