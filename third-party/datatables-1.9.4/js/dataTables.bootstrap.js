52a295822d9deb7632d6a29d7658fda656d44d6c
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-10-04 18:08:39 +0200

    upgraded dataTables.bootstrap.{js,css} for bootstrap 3 from dataTables github as-is

--- third-party/datatables-1.9.4/js/dataTables.bootstrap.js
+++ third-party/datatables-1.9.4/js/dataTables.bootstrap.js
@@ -1,11 +1,13 @@
 /* Set the defaults for DataTables initialisation */
 $.extend( true, $.fn.dataTable.defaults, {
-	"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+	"sDom": "<'row'<'col-xs-6'l><'col-xs-6'f>r>t<'row'<'col-xs-6'i><'col-xs-6'p>>",
 	"sPaginationType": "bootstrap",
 	"oLanguage": {
 		"sLengthMenu": "_MENU_ records per page"
 	}
 } );
+
+
 
 
 /* Default class modification */
@@ -43,8 +45,8 @@ $.extend( $.fn.dataTableExt.oPagination, {
 				}
 			};
 
-			$(nPaging).addClass('pagination').append(
-				'<ul>'+
+			$(nPaging).append(
+				'<ul class="pagination">'+
 					'<li class="prev disabled"><a href="#">&larr; '+oLang.sPrevious+'</a></li>'+
 					'<li class="next disabled"><a href="#">'+oLang.sNext+' &rarr; </a></li>'+
 				'</ul>'
@@ -118,7 +120,7 @@ if ( $.fn.DataTable.TableTools ) {
 	$.extend( true, $.fn.DataTable.TableTools.classes, {
 		"container": "DTTT btn-group",
 		"buttons": {
-			"normal": "btn",
+			"normal": "btn btn-default",
 			"disabled": "disabled"
 		},
 		"collection": {
