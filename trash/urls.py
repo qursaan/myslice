4c8eb874d9d6943df2922a506f63c7dcf56f1c17
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-11-30 11:00:16 +0100

    a simple view in trash/ for displaying only one querytable for a slice

--- trash/urls.py
+++ trash/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls           import patterns, include, url
+import trash.simpleview
 
 urlpatterns = patterns('',
                        url(r'^tab/?$',          'trash.sampleviews.tab_view'),
                        (r'^scroll/?$',          'trash.sampleviews.scroll_view'),
                        (r'^plugin/?$',          'trash.pluginview.test_plugin_view'),
                        (r'^dashboard/?$',       'trash.dashboard.dashboard_view'),
+                       url(r'^simple/(?P<slicename>[\w\.]+)/?$', trash.simpleview.SimpleView.as_view()),
 )
