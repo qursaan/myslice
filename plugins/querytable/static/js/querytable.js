5c4d20a3863a84f437a3045bc48e11b0e911a241
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-12-04 21:34:50 +0100

    reversing previous commit that only eemed to work actually all the columns were showing up, but only the top rows were visible

--- plugins/querytable/static/js/querytable.js
+++ plugins/querytable/static/js/querytable.js
@@ -85,7 +85,6 @@
 	    this.slick_options = {
 		enableCellNavigation: false,
 		enableColumnReorder: true,
-		explicitInitialization: true,		
 	    };
 
 	    this.slick_data=[];
@@ -338,7 +337,7 @@
 	    if (debug) messages.debug("1-shot initializing dataTables content with " + this.slick_data.length + " lines");
 	    var start=new Date();
 	    this.slick_grid.setData (this.slick_data, true);
-	    this.slick_grid.init();
+	    this.slick_grid.render();
 	    var duration=new Date()-start;
 	    if (debug) messages.debug("setData+render took " + duration + " ms");
 	    if (debug_deep) {
