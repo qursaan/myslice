c8c35ed0d987d0ad6b903c74d4688dbe19d1cd78
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-12-16 12:19:16 +0100

    move commented code about the ‘where am I a PI’ question into topmenuvalidation plugin

--- plugins/topmenuvalidation/__init__.py
+++ plugins/topmenuvalidation/__init__.py
@@ -33,3 +33,40 @@ Query.get('ple:user').filter_by('user_hrn', '==', '$user_hrn').select('pi_author
 
     def json_settings_list (self):
         return [ 'query_uuid', 'button_domid', ]
+
+#################### here is an extract previously in topmenu.py
+###        # ** Where am I a PI **
+###        # For this we need to ask SFA (of all authorities) = PI function
+###        user_query  = Query().get('local:user').select('config','email')
+###        user_details = execute_query(request, user_query)
+###
+###        # Required: the user must have an authority in its user.config
+###        # XXX Temporary solution
+###        # not always found in user_details...
+###        config={}
+#### Deactivated until fixed 
+####        if user_details is not None:
+####            for user_detail in user_details:
+####                #email = user_detail['email']
+####                if user_detail['config']:
+####                    config = json.loads(user_detail['config'])
+####            user_detail['authority'] = config.get('authority',"Unknown Authority")
+####            print "topmenu: %s", (user_detail['authority'])
+####            if user_detail['authority'] is not None:
+####                sub_authority = user_detail['authority'].split('.')
+####                root_authority = sub_authority[0]
+####                pi_authorities_query = Query.get(root_authority+':user').filter_by('user_hrn', '==', '$user_hrn').select('pi_authorities')
+####        else:
+####            pi_authorities_query = Query.get('user').filter_by('user_hrn', '==', '$user_hrn').select('pi_authorities')
+####        try:
+####            pi_authorities_tmp = execute_query(request, pi_authorities_query)
+####        except:
+####            pi_authorities_tmp = set()
+####        pi_authorities = set()
+####        for pa in pi_authorities_tmp:
+####            if 'pi_authorities' in pa:
+####                pi_authorities |= set(pa['pi_authorities'])
+####        print "pi_authorities =", pi_authorities
+####        if len(pi_authorities) > 0:
+####            result.append({'label':'Validation', 'href': '/portal/validate/'})
+###        result.append({'label':'Validation', 'href': '/portal/validate/'})
