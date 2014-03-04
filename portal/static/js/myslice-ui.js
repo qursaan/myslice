27d397e29be7415951001c530a0cf31787ad8d83
root <root@Portable>
2014-03-04 19:18:06 +0100

    Testbed list: Add coloration when checkbox active

--- portal/static/js/myslice-ui.js
+++ portal/static/js/myslice-ui.js
@@ -9,33 +9,42 @@ $(document).ready(function() {
 	
 	/* Testbeds list */
 	$.post("/rest/network/", { "fields" : ["network_hrn", "network_longname", "description"]}, function(data) {
-		var testbed_data = [];
-		var testbed_row = "<thead>";
-		testbed_row += "<tr>";
-		testbed_row += "<th id=testbed_check><input type=\"checkbox\" name=\"network_hrn\" value=\"all\"/></th>";
-		testbed_row += "<th id=testbed_icon></th>";
-		testbed_row += "<th>network_hrn</th>";
-		testbed_row += "<th>Full name</th>";
-		testbed_row += "<th>Description</th>";
-		testbed_row += "</tr>";
-		testbed_row += "</thead>";
-		testbed_data.push(testbed_row);
-		$.each( data, function(key, val) {
-			testbed_row = "<tr data-keys=\""+val.network_hrn+"\">"
-			testbed_row += "<td><input type=\"checkbox\" name=\"network_hrn\" value=\""+val.network_hrn+"\"/></td>";
-			testbed_row += "<td><img src='/static/img/testbeds/"+val.network_hrn+".png' alt='' /></td>";
-			testbed_row += "<td>"+val.network_hrn+"</td>";
-			testbed_row += "<td>"+val.network_longname+"</td>";
-			testbed_row += "<td>"+val.description+"</td>";
+			var testbed_data = [];
+			var testbed_row = "<thead>";
+			testbed_row += "<tr>";
+			testbed_row += "<th id=testbed_check><input type=\"checkbox\" name=\"network_hrn\" value=\"all\"/></th>";
+			testbed_row += "<th id=testbed_icon></th>";
+			testbed_row += "<th>network_hrn</th>";
+			testbed_row += "<th>Full name</th>";
+			testbed_row += "<th>Description</th>";
+			testbed_row += "</tr>";
 			testbed_row += "</thead>";
-
 			testbed_data.push(testbed_row);
-		});
-	$("table#testbedList").html(testbed_data.join(''));
-	$("div#testbed-list-loaded").css("display","block");
-	$("div#testbed-list-loading").css("display","none");
-
+			$.each( data, function(key, val) {
+				testbed_row = "<tr data-keys=\""+val.network_hrn+"\" class=\"odd\">"
+				testbed_row += "<td><input type=\"checkbox\" name=\"network_hrn\" value=\""+val.network_hrn+"\"/></td>";
+				testbed_row += "<td><img src='/static/img/testbeds/"+val.network_hrn+".png' alt='' /></td>";
+				testbed_row += "<td>"+val.network_hrn+"</td>";
+				testbed_row += "<td>"+val.network_longname+"</td>";
+				testbed_row += "<td>"+val.description+"</td>";
+				testbed_row += "</thead>";
+	
+				testbed_data.push(testbed_row);
+			});
+		$("table#testbedList").html(testbed_data.join(''));
+		$("div#testbed-list-loaded").css("display","block");
+		$("div#testbed-list-loading").css("display","none");
 			
+		$("input[type=checkbox]").click(function() {
+			var cnt = 0;
+			var id = $(this).val();
+			var row = $(this).parent().parent()
+			if (row.hasClass("active")) {
+				row.removeClass("active");
+			} else {
+				row.addClass("active");
+				}
+		});
 	});
 	
 	$("#objectList").load("/table/resource/", {"fields" : ["hostname","hrn","country","type"], "options": ["checkbox"] }, function(data) {
