ba407936360d6bf5b17dee7c48062ceee9f2e40d
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2014-03-04 12:29:24 +0100

    added tab navigation

--- portal/static/js/onelab_slice-resource-view.js
+++ portal/static/js/onelab_slice-resource-view.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
+	$('ul.nav-resources a').click(function() {
+        $('ul.nav-resources li').removeClass('active');
+        $(this).parent().addClass('active');
+        $('div.panel').hide();
+        $('div#'+$(this).data('panel')).show();
+    });
 });
