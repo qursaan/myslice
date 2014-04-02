c46820bd25418529d8379b80e275597c0ef11100
Yasin <mohammed-yasin.rahman@lip6.fr>
2014-04-02 12:44:45 +0200

    Deactivated sfa query.

--- portal/slicetabusers.py
+++ portal/slicetabusers.py
@@ -22,10 +22,10 @@ class SliceUserView (LoginRequiredView, ThemeView):
     
     def get(self, request, slicename):
         if request.user.is_authenticated():
-            user_query  = Query().get('user').select('user_hrn','parent_authority').filter_by('user_hrn','==','$user_hrn')
-            user_details = execute_query(self.request, user_query)
+            #user_query  = Query().get('user').select('user_hrn','parent_authority').filter_by('user_hrn','==','$user_hrn')
+            #user_details = execute_query(self.request, user_query)
             # if sfa returns None
-            if user_details[0]['parent_authority'] is None:
+            #if user_details[0]['parent_authority'] is None:
                 # find in local DB 
                 user_query  = Query().get('local:user').select('config')
                 user_details = execute_query(request, user_query)
