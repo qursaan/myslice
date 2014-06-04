bd4dc8dbf249c10778137051c66c193b086095db
Yasin <root@lamia.ipv6.lip6.fr>
2014-06-04 11:20:07 -0500

    disregard

--- portal/registrationview.py
+++ portal/registrationview.py
@@ -66,7 +66,7 @@ class RegistrationView (FreeAccessView, ThemeView):
                 'password'      : wsgi_request.POST.get('password',      ''),
                 'current_site'  : current_site,
                 'email_hash'    : email_hash,
-                'validation_link': 'http://' + current_site + '/portal/email_activation/'+ email_hash
+                'validation_link': 'https://' + current_site + '/portal/email_activation/'+ email_hash
             }
 
             # Construct user_hrn from email (XXX Should use common code)
