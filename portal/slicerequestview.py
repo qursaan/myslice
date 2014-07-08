b932fc56d413781f414819c67ade7e69b707d1fb
Yasin <mohammed-yasin.rahman@lip6.fr>
2014-07-08 11:02:13 +0200

    jquery-ui added to specific pages

--- portal/slicerequestview.py
+++ portal/slicerequestview.py
@@ -70,6 +70,7 @@ class SliceRequestView (LoginRequiredAutoLogoutView, ThemeView):
 
         # Page rendering
         page = Page(wsgi_request)
+        page.add_js_files  ( [ "js/jquery.validate.js", "js/jquery-ui.js" ] )
         page.add_css_files ( [ "https://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" ] )
 
         if method == 'POST':
