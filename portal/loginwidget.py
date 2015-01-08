6e20d79cde8519615fd672e677966e0ef7b5a0f3
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2015-01-08 18:50:29 +0100

    csrf support

--- portal/loginwidget.py
+++ portal/loginwidget.py
@@ -1,14 +1,12 @@
-from django.template                 import RequestContext
-from django.shortcuts                import render_to_response
+from django.core.context_processors import csrf
+from django.shortcuts               import render_to_response
 
 from django.views.generic.base      import TemplateView
-from unfold.loginrequired import FreeAccessView
-
-from django.http import HttpResponse
-from django.shortcuts import render
+from unfold.loginrequired           import FreeAccessView
 
 class LoginWidget(FreeAccessView):
     
     def get(self, request):
         env = {}
-        return render_to_response("loginwidget.html", env, context_instance=RequestContext(request))
+        env.update(csrf(request))
+        return render_to_response("loginwidget.html", env)
