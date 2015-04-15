3454cb65d320d7aae6872cacf0163417326229c5
Loic Baron <loic.baron@lip6.fr>
2015-04-16 00:24:54 +0200

    Scheduler: adding/removing resources enforce warnings and recount number of unconfigured resources, still work to be done on the SUCCESS/FAILURE feedback

--- plugins/filter_status/__init__.py
+++ plugins/filter_status/__init__.py
@@ -2,9 +2,12 @@ from unfold.plugin import Plugin
 
 class FilterStatusPlugin(Plugin):
     
-    def __init__ (self, query=None, **settings):
+    def __init__ (self, query=None, query_lease=None, **settings):
         Plugin.__init__ (self, **settings)
         self.query              = query
+
+        self.query_lease = query_lease
+        self.query_lease_uuid = query_lease.query_uuid
 
     def template_file (self):
         return "filter_status.html"
@@ -24,7 +27,7 @@ class FilterStatusPlugin(Plugin):
         # query_uuid will pass self.query results to the javascript
         # and will be available as "record" in :
         # on_new_record: function(record)
-        return ['plugin_uuid', 'domid', 'query_uuid']
+        return ['plugin_uuid', 'domid', 'query_uuid', 'query_lease_uuid']
 
     def export_json_settings (self):
         return True
