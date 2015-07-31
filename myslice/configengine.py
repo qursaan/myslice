c21dc8a15adb119f7335d40e73c930796a0dd38f
Loic Baron <loic.baron@lip6.fr>
2015-07-31 17:46:12 +0200

    Singleton fixed

--- myslice/configengine.py
+++ myslice/configengine.py
@@ -19,7 +19,25 @@ from myslice.settings import ROOT
 #api_key=theapikeyasprovidedbygoogle
 
 # use a singleton instead of staticmethods
-from myslice.util.singleton    import Singleton
+#-------------------------------------------------------------------------
+# Class Singleton
+#
+# Classes that inherit from Singleton can be instanciated only once 
+#-------------------------------------------------------------------------
+
+class Singleton(type):
+    def __init__(cls, name, bases, dic):
+        super(Singleton,cls).__init__(name,bases,dic)
+        cls.instance=None
+
+    def __call__(cls, *args, **kw):
+        if cls.instance is None:
+            cls.instance=super(Singleton,cls).__call__(*args,**kw)
+        return cls.instance
+
+
+# See also
+# http://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
 
 class ConfigEngine(object):
     __metaclass__ = Singleton
